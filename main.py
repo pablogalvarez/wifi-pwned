fdf8505119b685a4467cdda95509b6ea87757d39
Pablo Gonzalez <pablo.glez.alvarez00@gmail.com>
2024-01-21 18:22:53 +0000

    Finish loading parameters to crack

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import json
 
 from common_functions import write_log
 
-WIFI_CRACK_DICTIONARY: str = ''
-INTERFACE: str | None = None
-SSID: str | None = None
+
+def load_configuration_parameters() -> tuple[str | None, str | None, str | None]:
+    with open('configuration.json') as f:
+        main_configuration: dict = json.load(f)
+
+        dictionary: str = ''
+        interface: str | None = None
+        bssid: str | None = None
+        try:
+            if 'dictionary' in main_configuration and main_configuration['dictionary']:
+                dictionary = main_configuration['dictionary']
+            else:
+                raise SystemExit
+        except SystemExit:
+            write_log('Dictionary for cracking passphrase not specified in configuration')
 
 
-with open('configuration.json') as f:
-    main_configuration: dict = json.load(f)
-    try:
-        if 'dictionary' in main_configuration and main_configuration['dictionary']:
-            WIFI_CRACK_DICTIONARY = main_configuration['dictionary']
-        else:
-            raise SystemExit
-    except SystemExit:
-        write_log('Dictionary for cracking passphrase not specified in configuration')
+        if 'interface' in main_configuration and main_configuration['interface']:
+            interface = main_configuration['interface']
 
+        if 'bssid' in main_configuration and main_configuration['bssid']:
+            bssid = main_configuration['bssid']
 
-    if 'interface' in main_configuration and main_configuration['interface']:
-        INTERFACE = main_configuration['interface']
+        return dictionary, interface, bssid
+
+    
+if __name__ == '__main__':
+    WIFI_CRACK_DICTIONARY, INTERFACE, BSSID = load_configuration_parameters()
     
