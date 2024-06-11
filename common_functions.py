633a37bf2827e9432ee14b299552a33367ae153a
Pablo Gonzalez <pablo.glez.alvarez00@gmail.com>
2024-06-11 06:00:53 +0000

    Refactor code

--- common_functions.py
+++ common_functions.py
@@ -1,6 +1,13 @@
 import datetime
+import json
 
 def write_log(description: str) -> None:
     with open('wifi-pwned.log', 'a') as f:
         now: str = datetime.datetime.strftime(datetime.datetime.now(), '%d/%m/%Y %H:%M:%S')
         f.write(f'{now} {description}\n')
+
+
+def get_config_field(field: str) -> str:
+    with open('configuration.json') as f:
+        config_fields = json.load(f)
+        return config_fields.get(field)
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import json
 import sys
 
-from common_functions import write_log
+from common_functions import get_config_field, write_log
 
 from components.airmon import Airmon
 
-if __name__ == '__main__':
-    
-    """Setting interface in monitor mode"""
+
+SUCCESSFULL_PREFIX = '[+]'
+ERROR_PREFIX = '[!]'
+
+
+def check_config_file():
     try:
         with open('configuration.json') as f:
-            configuration_params: dict = json.load(f)
-            interface = configuration_params.get('interface')
-
+            configuration_fields = json.load(f)
+            mandatory_fields = ['interface', 'ssid']
+            abort_program = False
+            for field in mandatory_fields:
+                if not field in configuration_fields:
+                    write_log(f'{ERROR_PREFIX} Configuration file "configuration.json" is uncomplete. Field "{field}" not found')
+                    abort_program = True
+            if abort_program:
+                sys.exit(1)
+            
     except FileNotFoundError:
-        write_log('Fichero de configuracion no encontrado "configuration.json"')
+        write_log(f'{ERROR_PREFIX} Configuration file "configuration.json" does not exists')
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    # Check if configuration file exists and it has every mandatory key
+    check_config_file()
+    
+    # Setting interface in monitor mode
+    interface = get_config_field('interface')
 
     airmon: Airmon = Airmon()
     airmon.check(kill=True)
